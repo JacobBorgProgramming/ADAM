59da1523a09846c4566c3c94c84fa8e1b82001a4
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-03-24 12:22:54 -0700

    updated page structures

---
+++ footermenu.js
@@ -0,0 +1,10 @@
+
+function footermenu(){
+
+    document.getElementById("footermenu").innerHTML = '    <center><p>© Jacob Borg 2023</p> <p> <a href="./about.html">About</a><a style="margin-left: 15px;" href="./changelog.html">Change Log</a> </p> </center>'
+
+}
+
+
+
+
