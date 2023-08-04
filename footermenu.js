24f4f1fb0abb46bedd5e4e1f3e8f4d40f5bedd12
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-08-03 18:48:19 -0700

    ADAM privacy statement

--- footermenu.js
+++ footermenu.js
@@ -1,10 +1,6 @@
 
 function footermenu(){
 
-    document.getElementById("footermenu").innerHTML = '    <center><p>© Jacob Borg 2023</p> <p> <a href="./about.html">About</a><a style="margin-left: 15px;" href="./mememaker.html">Meme Maker</a><a style="margin-left: 15px;" href="./changelog.html">Change Log</a><a style="margin-left: 15px;" href="https://github.com/JacobBorgProgramming/ADAM" target="_blank">Github</a> </p> <p> <a href="https://bulma.io/" target="_blank"> <img style="margin-top: 15px;" width="150px" src="./made-with-bulma--white.png"> </a> </p> </center>'
+    document.getElementById("footermenu").innerHTML = '    <center><p>© Jacob Borg 2023</p> <p> <a href="./about.html">About</a><a style="margin-left: 15px;" href="./mememaker.html">Meme Maker</a><a style="margin-left: 15px;" href="./changelog.html">Change Log</a><a style="margin-left: 15px;" href="./privacy.html">Privacy</a><a style="margin-left: 15px;" href="https://github.com/JacobBorgProgramming/ADAM" target="_blank">Github</a> </p> <p> <a href="https://bulma.io/" target="_blank"> <img style="margin-top: 15px;" width="150px" src="./made-with-bulma--white.png"> </a> </p> </center>'
 
 }
-
-
-
-
---
+++ privacy.js
@@ -0,0 +1,6 @@
+
+function privacystatement(){
+
+    document.getElementById("privacy").innerHTML = '<article style="margin-top: 25px; margin-bottom: 25px" class="message"><div class="message-body">By using ADAM, you are cool with our privacy stuff (You agree and approve of our privacy practices). <a href="./privacy.html"><strong>Read about privacy and ADAM</strong></a></div></article>'
+
+}
